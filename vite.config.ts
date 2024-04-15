e505a9158527e2c40b2aca42e12c42dabb9f0a83
Kushal Niroula <niroulaKushal31@gmail.com>
2024-04-15 07:41:46 +0000

    chore: generate ts types

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-
+import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [
+        dts({
+            outDir: "./dist"
+        })
+    ],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
